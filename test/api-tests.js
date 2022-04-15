05b2ba6423725fa5bd1d6014aa0508c15d9e82d4
liliia <liliyameister@gmail.com>
2022-04-15 12:07:12 +0200

    added two tests

---
+++ test/api-tests.js
@@ -0,0 +1,28 @@
+const request = require('supertest');
+const app = require('express')();
+const Record = require('../models/record');
+const User = require('../models/user');
+
+describe('Test a GET request for /users', function (){
+    it('users should not be empty', function(done){
+        const response = request(app).get('/users').send();
+        request(app)
+        .get('/users')
+        .set('Accept', 'application/json')
+        .expect(200, done);
+    });
+});
+
+describe('Test a POST request for /users', function(){
+    it('response should contain an object and status code 200', function(done){
+        const response = request(app).post('/users').send(
+            new User({
+                email: 'lili@gmail.com',
+                firstName: 'Liliia',
+                lastName: 'Allans',
+                birthDate: '10/12/1900'})
+        ).expect(response.status).toBe(200)
+        .expect(response.body).not('');
+    });
+    
+});
