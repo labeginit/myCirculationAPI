1aa3655ac2b0770d0d152a3fdf6442302fe3d296
liliia <liliyameister@gmail.com>
2022-04-26 18:06:25 +0200

    tests

--- test/api-tests.js
+++ test/api-tests.js
@@ -1,30 +1,36 @@
 const request = require('supertest');
-const app = require('express')();
+const app = require('express');
 const Record = require('../models/record');
 const User = require('../models/user');
-require('dotenv/config');
+const { assert } = require('chai');
 
-describe('Test a GET request for /users', function () {
-    console.log(process.env.DB_URL);
-    it('users should not be empty', function (done) {
-        const response = request(app).get('/users').send();
-        request(app)
-            .get('/users')
-            .set('Accept', 'application/json')
-            .expect(404, done);
-
-    });
-});
-/*
-describe('Test a POST request for /users', function(){
-    it('response should contain an object and status code 200', function(done){
-        const response = request(app).post('/users').send(
+describe('POST request to /register', function () {
+    it('response should contain an object and status code 200', function (done) {
+        this.timeout(5000);
+        const response = request(app).post('/register').send(
             new User({
                 email: 'lili@gmail.com',
-                firstName: 'Liliia',
-                lastName: 'Allans',
-                birthDate: '10/12/1900'})
-        ).expect(200, done);
+                firstName: 'NewUser',
+                lastName: 'Surname',
+                birthDate: '2000-07-06',
+                password: 'password'
+            })).expect(200, done);
+
+        assert(response._data._doc.email == 'lili@gmail.com');
+        done();
     });
-    
-});*/
+});
+
+describe('POST request to /login', function () {
+    it('response should contain an object and status code 200', function (done) {
+        this.timeout(5000);
+        const response = request(app).post('/login').send(
+            {
+                email: 'lili@gmail.com',
+                password: 'password'
+            }).expect(200, done);
+        done();
+    });
+});
+
+
