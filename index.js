93e2a1a3a5ed4cb45faeb344c2e44b9d1a8bef58
liliia <liliyameister@gmail.com>
2022-05-12 11:41:36 +0200

    cors off again

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ mongoose.connect(DB_URL, { useUnifiedTopology: true })
 
 app.use(express.json());
 app.use(cors());
-app.use(
+/*app.use(
     cors({
         origin: "https://calm-badlands-26710.herokuapp.com",
         credentials: true
     })
-);
+);*/
 app.use(cookieParser());
 app.use(session({
     secret: "625f088260800ba7daa61038",
